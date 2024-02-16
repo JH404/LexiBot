111855d44679e07cd55f5942efb98d23400335e0
JH404 <31189361+JH404@users.noreply.github.com>
2024-02-16 19:40:32 +0200

    Added functions
    Added handle_reactions() and fixed game rules.

--- LexiBot.py
+++ LexiBot.py
@@ -1,5 +1,4 @@
 import asyncio
-
 import discord
 from discord.ext import commands
 import os
@@ -12,7 +11,10 @@ intents.message_content = True
 bot = commands.Bot(command_prefix="!", intents=intents)
 
 last_user_message = None
+last_user_id = None
 wordchain_channel_id = None
+used_words = {}
+selected_reaction = None
 on_ready_message = """
 ** :chains: Welcome to play the Wordchain! :chains:  **
 
@@ -45,7 +47,40 @@ Sanan pituus vähintään 5 kirjainta.
 -
 """
 
-emoji_list = ['\U0001F95B', '\U00002615', '\U0001F379', '\U0001F37B']
+
+async def handle_reactions(message):
+    global selected_reaction
+    emoji_list = ['\U0001F95B', '\U00002615', '\U0001F378', '\U0001F37B']
+    for emoji in emoji_list:
+        await message.add_reaction(emoji)
+
+    def check(reaction, user):
+        return user != bot.user and reaction.message.id == message.id
+
+    reaction, user = await bot.wait_for('reaction_add', check=check)
+    selected_reaction = reaction.emoji
+    if reaction.emoji == '\U0001F95B':
+        await reaction.message.channel.send(f":milk: Okay, I'll go easy on you. First word is: **Maito**")
+    elif reaction.emoji == '\U00002615':
+        await reaction.message.channel.send(f':coffee: Too tired for hard one? First word is: **Kahvi**')
+    elif reaction.emoji == '\U0001F378':
+        await reaction.message.channel.send(f'_Tätä pelimuotoa ei vielä ole olemassa!_')
+        # :cocktail: Something tougher? As you wish! First word is: **Mojito**
+    elif reaction.emoji == '\U0001F37B':
+        await reaction.message.channel.send(f'_Tätä pelimuotoa ei vielä ole olemassa!_')
+        # :beers: You are not immortal you know? First word is: **Karhu**
+
+
+async def handle_rule_violation(channel):
+    global last_user_id, used_words
+    await channel.send(
+        f"**Sääntöjä rikottu!** _Peli nollataan 10 sekunnin kuluttua..._")
+    await asyncio.sleep(10)
+    await channel.purge()
+    last_user_id = None
+    used_words = {}
+    new_message = await channel.send(on_ready_message)
+    await handle_reactions(new_message)
 
 
 @bot.event
@@ -68,8 +103,7 @@ async def on_ready():
         channel = await guild.create_text_channel("wordchain", overwrites=overwrites)
         wordchain_channel_id = channel.id
         message = await channel.send(on_ready_message)
-        for emoji in emoji_list:
-            await message.add_reaction(emoji)
+        await handle_reactions(message)
 
 
 @bot.event
@@ -94,21 +128,27 @@ async def on_message_edit(before, after):
 
 @bot.event
 async def on_message(message):
-    global last_user_message, wordchain_channel_id
-    if message.channel.id != wordchain_channel_id:
+    global last_user_message, wordchain_channel_id, last_user_id, used_words, selected_reaction
+    if message.channel.id != wordchain_channel_id or selected_reaction is None:
         return
     if message.author == bot.user:
         return
     if not message.author.bot:
+        if last_user_id == message.author.id:
+            await message.channel.send(f"Voit lähettää vain yhden viestin kerrallaan!")
+            await handle_rule_violation(message.channel)
+            return
+        if message.content in used_words:
+            await message.channel.send(f"Sana **'{message.content}'** on jo käytetty!")
+            await handle_rule_violation(message.channel)
+            return
         last_user_message = message
+        last_user_id = message.author.id
+        used_words[message.content] = True
     if len(message.content.split()) > 1:
-        await message.channel.send(
-            f"Sääntöjä rikottu! Voit lähettää vain yhden sanan kerrallaan. Peli nollataan 15 sekunnin kuluttua...")
-        await asyncio.sleep(15)
-        await message.channel.purge()
-        new_message = await message.channel.send(on_ready_message)
-        for emoji in emoji_list:
-            await new_message.add_reaction(emoji)
+        await message.channel.send(f"Viestisi voi sisältää vain yhden sanan!")
+        await handle_rule_violation(message.channel)
+        return
 
 
 @bot.event

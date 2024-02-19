678f7e113d657e337e9dbc42dd72a918224bb361
JH404 <31189361+JH404@users.noreply.github.com>
2024-02-19 02:04:29 +0200

    Modes working, added texts

--- LexiBot.py
+++ LexiBot.py
@@ -15,36 +15,42 @@ last_user_id = None
 wordchain_channel_id = None
 used_words = {}
 selected_reaction = None
+parity = 0
+is_handling_violation = False
+user_message_count = 0
+total_message_count = 0
 on_ready_message = """
-** :chains: Welcome to play the Wordchain! :chains:  **
+** :chains: Tervetuloa pelaamaan Sanaketjua :chains:  **
 
 **Aloita uusi peli reagoimalla tähän viestiin haluamasi pelimuotoa vastaavalla emojilla!**
 
 :ok_hand: Sama käyttäjä ei voi vastata kuin kerran putkeen.
 :v: Samoja sanoja ei voi käyttää uudelleen.
 :metal: Voit kirjoittaa vain yhden sanan kerrallaan.
+:hand_with_index_finger_and_thumb_crossed: Ei numeroiden eikä erikoismerkkien käyttöä.
 :pinched_fingers: Helpotat muiden peliä kun et muokkaa tai poista vastauksiasi.
 :muscle: Sääntöjä rikkoessa botti aloittaa uuden pelin.
 
-**Wordchainin eri pelimuotojen säännöt alapuolella:**
+**Sanaketjun eri pelimuotojen säännöt alapuolella:**
 
 :milk:  **Milk mode:**
-Uusi sana alkaa edellisen sanan viimeisellä kirjaimella.
+- Seuraavan sanan tulee alkaa edellisen sanan viimeisellä kirjaimella.
 
 :coffee:  **Coffee mode:**
-Uusi sana alkaa edellisen sanan kahdella viimeisellä kirjaimella.
-Sanan pituus vähintään 4 kirjainta.
+- Seuraavan sanan tulee alkaa edellisen sanan kahdella viimeisellä kirjaimella.
+- Sanan pituus on oltava vähintään 4 kirjainta.
 
 :cocktail:  **Mojito mode:**
-Uusi sana alkaa edellisen sanan viimeisellä kirjaimella.
-Uusi sana sisältää 2 samaa kirjainta edellisestä sanasta lukuunottamatta viimeistä kirjainta.
-Sanan pituus vähintään 4 kirjainta.
+- Seuraavan sanan tulee alkaa edellisen sanan kahdella viimeisellä kirjaimella.
+- Joka toisen sanan merkkimäärä tulee olla on parillinen ja joka toisen pariton.
+- Sanan pituus on oltava vähintään 4 kirjainta.
 
 :beers:  **Booze mode:**
-Uusi sana alkaa edellisen sanan kahdella viimeisellä kirjaimella.
-Uusi sana sisältää 2 samaa kirjainta edellisestä sanasta lukuunottamatta kahta viimeistä kirjainta.
-Sanan pituus vähintään 5 kirjainta.
--
+- Seuraavan sanan tulee alkaa edellisen sanan kahdella viimeisellä kirjaimella.
+- Joka toisen sanan merkkimäärä tulee olla on parillinen ja joka toisen pariton.
+- Sanassa ei saa olla samaa kirjainta peräkkäin.
+- Sanan pituus on oltava vähintään 5 kirjainta.
+-------------------------------------------------------------------------------
 """
 
 
@@ -60,33 +66,119 @@ async def handle_reactions(message):
     reaction, user = await bot.wait_for('reaction_add', check=check)
     selected_reaction = reaction.emoji
     if reaction.emoji == '\U0001F95B':
-        await reaction.message.channel.send(f":milk: Okay, I'll go easy on you. First word is: **Maito**")
-        last_user_message = "maito"
-        used_words["maito"] = True
+        await reaction.message.channel.send(f":milk: Okay, I'll go easy on you.")
     elif reaction.emoji == '\U00002615':
-        await reaction.message.channel.send(f':coffee: Too tired for hard one? First word is: **Kahvi**')
-        last_user_message = "kahvi"
-        used_words["kahvi"] = True
+        await reaction.message.channel.send(f':coffee: Too tired for hard one?')
     elif reaction.emoji == '\U0001F378':
-        await reaction.message.channel.send(f':cocktail: Something tougher? As you wish! First word is: **Mojito**')
-        last_user_message = "mojito"
-        used_words["mojito"] = True
+        await reaction.message.channel.send(f':cocktail: Something tougher? As you wish!')
     elif reaction.emoji == '\U0001F37B':
-        await reaction.message.channel.send(f':beers: You are not immortal you know? First word is: **Olut**')
-        last_user_message = "olut"
-        used_words["olut"] = True
+        await reaction.message.channel.send(f':beers: You are not immortal you know?')
 
 
 async def handle_rule_violation(channel):
-    global last_user_id, used_words
+    global last_user_id, used_words, last_user_message, selected_reaction, parity, is_handling_violation, user_message_count, total_message_count
+    is_handling_violation = True
     await channel.send(
         f"**Sääntöjä rikottu!** _Peli nollataan 10 sekunnin kuluttua..._")
     await asyncio.sleep(10)
+    while True:
+        deleted = await channel.purge(limit=100)
+        if len(deleted) < 100:
+            break
     await channel.purge()
+
+    last_user_message = None
     last_user_id = None
     used_words = {}
+    selected_reaction = None
+    parity = 0
+    user_message_count = 0
+    total_message_count = 0
+
     new_message = await channel.send(on_ready_message)
+    await new_message.pin()
     await handle_reactions(new_message)
+    is_handling_violation = False
+
+
+async def milk_mode(message, lower_message_content, last_user_message):
+    if last_user_message is not None and lower_message_content[0] != last_user_message.content[-1].lower():
+        await message.channel.send(f"Seuraavan sanan tulee alkaa edellisen sanan viimeisellä kirjaimella.")
+        await handle_rule_violation(message.channel)
+        return False
+    return True
+
+
+async def coffee_mode(message, lower_message_content, last_user_message):
+    if len(lower_message_content) < 4:
+        await message.channel.send(f"Sanan pituus on oltava vähintään 4 kirjainta.")
+        await handle_rule_violation(message.channel)
+        return False
+    if last_user_message is not None and lower_message_content[:2] != last_user_message.content[-2:].lower():
+        await message.channel.send(f"Seuraavan sanan tulee alkaa edellisen sanan kahdella viimeisellä kirjaimella.")
+        await handle_rule_violation(message.channel)
+        return False
+    return True
+
+
+async def mojito_mode(message, lower_message_content):
+    global last_user_message, last_user_id, used_words, parity
+    if len(lower_message_content) < 4:
+        await message.channel.send(f"Sanan pituus on oltava vähintään 4 kirjainta.")
+        await handle_rule_violation(message.channel)
+        return False
+    if last_user_message is None:
+        last_user_message = message
+        last_user_id = message.author.id
+        used_words[lower_message_content] = True
+        parity = 1 - len(lower_message_content) % 2
+        return True
+    else:
+        if lower_message_content[:2] != last_user_message.content[-2:].lower():
+            await message.channel.send(f"Seuraavan sanan tulee alkaa edellisen sanan kahdella viimeisellä kirjaimella.")
+            await handle_rule_violation(message.channel)
+            return False
+        if len(lower_message_content) % 2 != parity:
+            await message.channel.send(f"Joka toisen sanan merkkimäärä tulee olla on parillinen ja joka toisen pariton.")
+            await handle_rule_violation(message.channel)
+            return False
+    last_user_message = message
+    last_user_id = message.author.id
+    used_words[lower_message_content] = True
+    parity = 1 - parity
+    return True
+
+
+async def booze_mode(message, lower_message_content):
+    global last_user_message, last_user_id, used_words, parity
+    if len(lower_message_content) < 5:
+        await message.channel.send(f"Sanan pituus on oltava vähintään 5 kirjainta.")
+        await handle_rule_violation(message.channel)
+        return False
+    if any(lower_message_content[i] == lower_message_content[i + 1] for i in range(len(lower_message_content) - 1)):
+        await message.channel.send(f"Sanassa ei saa olla samaa kirjainta peräkkäin.")
+        await handle_rule_violation(message.channel)
+        return False
+    if last_user_message is None:
+        last_user_message = message
+        last_user_id = message.author.id
+        used_words[lower_message_content] = True
+        parity = 1 - len(lower_message_content) % 2
+        return True
+    else:
+        if lower_message_content[:2] != last_user_message.content[-2:].lower():
+            await message.channel.send(f"Seuraavan sanan tulee alkaa edellisen sanan kahdella viimeisellä kirjaimella.")
+            await handle_rule_violation(message.channel)
+            return False
+        if len(lower_message_content) % 2 != parity:
+            await message.channel.send(f"Joka toisen sanan merkkimäärä tulee olla on parillinen ja joka toisen pariton.")
+            await handle_rule_violation(message.channel)
+            return False
+    last_user_message = message
+    last_user_id = message.author.id
+    used_words[lower_message_content] = True
+    parity = 1 - parity
+    return True
 
 
 @bot.event
@@ -109,6 +201,7 @@ async def on_ready():
         channel = await guild.create_text_channel("wordchain", overwrites=overwrites)
         wordchain_channel_id = channel.id
         message = await channel.send(on_ready_message)
+        await message.pin()
         await handle_reactions(message)
 
 
@@ -134,15 +227,26 @@ async def on_message_edit(before, after):
 
 @bot.event
 async def on_message(message):
-    global last_user_message, wordchain_channel_id, last_user_id, used_words, selected_reaction
+    global last_user_message, wordchain_channel_id, last_user_id, used_words, selected_reaction, is_handling_violation, user_message_count, total_message_count
+    if is_handling_violation:
+        return
     if message.channel.id != wordchain_channel_id or selected_reaction is None:
         return
     if message.author == bot.user:
         return
     if not message.author.bot:
+        user_message_count += 1
+        total_message_count += 1
+        if user_message_count >= 10:
+            await message.channel.send(f"**:chains::fire:{total_message_count} STREAK!!!:fire::chains:**")
+            user_message_count = 0
         lower_message_content = message.content.lower()
+        if not lower_message_content.isalpha():
+            await message.channel.send(f"Sanasi ei saa sisältää numeroita tai erikoismerkkejä!")
+            await handle_rule_violation(message.channel)
+            return
         if last_user_id == message.author.id:
-            await message.channel.send(f"Voit lähettää vain yhden viestin kerrallaan!")
+            await message.channel.send(f"Voit lähettää vain yhden sanan vuorollasi!")
             await handle_rule_violation(message.channel)
             return
         if lower_message_content in used_words:
@@ -150,37 +254,21 @@ async def on_message(message):
             await handle_rule_violation(message.channel)
             return
         if selected_reaction == '\U0001F95B':  # Milk mode
-            if last_user_message is not None and lower_message_content[0] != last_user_message.content[-1].lower():
-                await handle_rule_violation(message.channel)
+            if not await milk_mode(message, lower_message_content, last_user_message):
                 return
         elif selected_reaction == '\U00002615':  # Coffee mode
-            if (last_user_message is not None and len(lower_message_content) >= 4
-                    and lower_message_content[:2] != last_user_message.content[-2:].lower()):
-                await handle_rule_violation(message.channel)
+            if not await coffee_mode(message, lower_message_content, last_user_message):
                 return
         elif selected_reaction == '\U0001F378':  # Mojito mode
-            if (last_user_message is not None and len(lower_message_content) >= 4
-                    and lower_message_content[0] != last_user_message.content[-1].lower()):
-                common_letters = set(lower_message_content[:-1]).intersection(
-                    set(last_user_message.content[:-1].lower()))
-                if len(common_letters) < 2:
-                    await handle_rule_violation(message.channel)
-                    return
+            if not await mojito_mode(message, lower_message_content):
+                return
         elif selected_reaction == '\U0001F37B':  # Booze mode
-            if (last_user_message is not None and len(lower_message_content) >= 5
-                    and lower_message_content[:2] != last_user_message.content[-2:].lower()):
-                common_letters = set(lower_message_content[:-2]).intersection(
-                    set(last_user_message.content[:-2].lower()))
-                if len(common_letters) < 2:
-                    await handle_rule_violation(message.channel)
-                    return
+            if not await booze_mode(message, lower_message_content):
+                return
+        await message.add_reaction(selected_reaction)
         last_user_message = message
         last_user_id = message.author.id
         used_words[lower_message_content] = True
-    if len(lower_message_content.split()) > 1:
-        await message.channel.send(f"Viestisi voi sisältää vain yhden sanan!")
-        await handle_rule_violation(message.channel)
-        return
 
 
 @bot.event
